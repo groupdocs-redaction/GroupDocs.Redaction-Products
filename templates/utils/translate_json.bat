01370073a64ee0c34c0effb0b32f39c3b3361fde
igor.zubarev <igor.zubarev@aspose.com>
2023-06-30 21:36:32 +0300

    update translation tools

---
+++ templates/utils/translate_json.bat
@@ -0,0 +1,11 @@
+@echo on
+set lang=%2
+set infile=%1.json
+set outfile=%1.%lang%.json
+set tempfile=%1.en.%lang%.json
+@echo translating %infile% into %lang%
+@echo %infile% -^> %tempfile% -^> %outfile%'
+
+Translation\Bin\net6.0\Res.Translator.exe -t aws -r ..\data\%infile% -d %lang% --overwrite
+if exist "..\data\%outfile%" del "..\data\%outfile%"
+ren "..\data\%tempfile%" "%outfile%"
